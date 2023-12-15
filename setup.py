d79cb8a0f5baf50400d162ca2c4f9fde064cab64
liu <zuiwoxing@qq.com>
2023-12-15 13:33:38 +0800

    文档说明

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="feign-python",
-    version="0.0.4",
-    author="黑肱",
+    version="0.0.5",
+    author="ldj",
     author_email="zuiwoxing@qq.com",
-    description="feign python实现",
+    description="feign python实现,提供python应用与java微服务体系集成，目前支持注册中心(nacos)以及非注册中心调用模式",
     long_description_content_type="text/markdown",
-    url="https://github.com/openandopen/feign-python",
+    url="https://github.com/openandopen/feign-python.git",
     packages= find_packages(),
     classifiers=[
         # 发展时期,常见的如下
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5 - Beta',
+        'Development Status :: 4 - Beta',
         # 开发的目标用户
         'Intended Audience :: Developers',
         # 属于什么类型
